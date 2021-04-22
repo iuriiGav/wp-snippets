0407950bc08c8a1b94a221ebd6b7ff083a947859
iuriiGav <iurii_gavryliuk@ukr.net>
2021-04-22 17:00:40 +0300

    add close in and out btn

---
+++ components/buttons/close-buttons/button-close-in-and-out/button-close-in-and-out.php
@@ -0,0 +1,10 @@
+  <a href="#" class="ig-in-and-out-close-btn">
+    <div class="ig-in-and-out-close-btn__in">
+      <div class="ig-in-and-out-close-btn__block"></div>
+      <div class="ig-in-and-out-close-btn__block"></div>
+    </div>
+    <div class="ig-in-and-out-close-btn__out">
+      <div class="ig-in-and-out-close-btn__block"></div>
+      <div class="ig-in-and-out-close-btn__block"></div>
+    </div>
+  </a>
